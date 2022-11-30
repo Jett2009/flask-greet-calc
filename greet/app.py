e58db2e204a4cfd53c453363eaa229b9e6af7dee
Braxton <Bjett2009@gmail.com>
2022-11-29 23:16:27 -0500

    commit

--- greet/app.py
+++ greet/app.py
@@ -1,2 +1,14 @@
-from Flask import Flask
+from flask import Flask
 app = Flask(__name__)
+
+@app.route('/welcome')
+def welcome():
+        return "welcome"
+        
+@app.route('/welcome/back')
+def welcome_back():
+    return "welcome back"
+
+@app.route('/welcome/home')
+def welcome_home():
+        return "welcome home"
